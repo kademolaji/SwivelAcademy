239a3155920461daa4fbd19d2b71c77766632c24
Kayode Ademolaji <k.ademolaji@televic.com>
2021-07-07 00:09:00 +0100

    Added base repository

--- Swivel.Commands/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Swivel.Commands
-{
-    public class Class1
-    {
-    }
-}
---
+++ Swivel.Repository/RepositoryBase.cs
@@ -0,0 +1,59 @@
+﻿using Swivel.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Swivel.Repository
+{
+    public interface IRepositoryBase<T>
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected AppDbContext _dbContext { get; set; }
+
+        public RepositoryBase(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return this._dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this._dbContext.Set<T>().Where(expression);
+        }
+
+        public void Create(T entity)
+        {
+            this._dbContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this._dbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this._dbContext.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            this._dbContext.SaveChanges();
+        }
+    }
+}
